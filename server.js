70357df84565d3d9ee08a39def92e64f63e06628
spirit109 <88921383+spirit109@users.noreply.github.com>
2025-09-12 23:23:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -42,5 +42,6 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on https://wps-backend-service.vercel.app`);
+
 });
